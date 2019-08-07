3dd5e40f980089819b5ea675f0ebea7b59613542
Edgar <mayedgar22@gmail.com>
2019-08-07 17:59:34 -0500

    Navigator and header

--- wp-content/themes/bethshalom/header.php
+++ wp-content/themes/bethshalom/header.php
@@ -7,4 +7,34 @@
     </head>
 
 <body <?php body_class();?>>
-<nav>Here's our nav menu</nav>
+<div class="header">
+  <img src="http://bethshalombz.org/blog2/wp-content/uploads/2010/02/brotherhood_logo1.jpg" alt="Beth Shalom Logo" align="left">
+  <h1>Beth Shalom B'Nai Zaken</h1>
+
+</div>
+
+<nav><!-- Navigation Bar --> 
+<ul>
+    <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">About Us</a>
+      <div class="dropdown-content">
+        <a href="#history">History</a>
+        <a href="#studies">Studies</a>
+        <a href="#media">Media</a>
+      <li><a href="#committees">Committees</a></li>     
+      <li><a href="#calendar">Calenedar</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#donate">Donate</a></li>
+      <li><a href="#contactus">Contact Us</a></li>
+      <li><a href="#blog">Blog</a></li>
+      <li class="dropdown">
+          <a href="javascript:void(0)" class="dropbtn">Resources</a>
+          <div class="dropdown-content">
+            <a href="#gallery">Gallery</a>
+            <a href="#lunarmonth">Lunar Month Learning</a>
+            <a href="#israelite">Israelite Black Jews</a>
+            </div>
+          </li>
+      </div>
+    </li>
+  </ul></nav>
