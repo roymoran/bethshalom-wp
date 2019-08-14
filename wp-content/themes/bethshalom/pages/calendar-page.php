d165eab54a75e8c59ef5a9a9c4d817b3bcad639a
Roy Moran <roy_moran1993@yahoo.com>
2019-08-14 14:53:01 -0500

    Calendar (#21)
    * add calendar page
    
    * add calendar page w/ embedded google calendar

---
+++ wp-content/themes/bethshalom/pages/calendar-page.php
@@ -0,0 +1,13 @@
+<section class="page-header">
+    <div>CALENDAR</div>
+    <hr>
+</section>
+
+<section class="info-section">
+    <div class="info-section-title">Torah & Shabbat Times for Chicago, Illinois:</div>
+    <p class="info-section-content">Candle lighting: 8:03pm on Friday, 19 Jul 2019<br>This week's Torah portion is Parashat Balak<br>Havdalah (50 min): 9:10pm on Saturday, 20 Jul 2019</p>
+</section>
+
+<section class="calendar-section">
+    <iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=cm95bW9yYW4yMEBnbWFpbC5jb20&amp;color=%237986CB&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=1&amp;showTabs=1&amp;showCalendars=0&amp;showTz=1" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</section>
