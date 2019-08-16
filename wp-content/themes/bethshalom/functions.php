33a39875efc88e596b2241939891a7b52b2dff82
Roy Moran <roy_moran1993@yahoo.com>
2019-08-16 08:41:47 +0200

    add home page

--- wp-content/themes/bethshalom/functions.php
+++ wp-content/themes/bethshalom/functions.php
@@ -1,9 +1,17 @@
-<?php 
+<?php
 
-function load_stylesheets() 
+function load_stylesheets()
 {
+    // Add Adobe Source San Pro Font
+    wp_enqueue_style('add_adobe_font', 'https://use.typekit.net/isd3dsn.css', false);
+
+    // Enque required wordpress stylesheet
     wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
     wp_enqueue_style('style');
+
+    // Enque stylesheet for homepage 
+    wp_register_style('home', get_template_directory_uri() . '/assets/stylesheets/home.css', array(), false, 'all');
+    wp_enqueue_style('home');
 }
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
---
+++ wp-content/themes/bethshalom/pages/home-page.php
@@ -0,0 +1,60 @@
+<section class="slider-section">
+    <div class="slider-info-container">
+        <div class="slider-info">
+            <div class="title">Welcome!</div>
+            <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        </div>
+    </div>
+</section>
+
+<section class="section home-section">
+    <div class="title">OUR MISSION</div>
+    <p class="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</p>
+</section>
+
+<section class="section services-section">
+    <div class="title">OUR SERVICES</div>
+    <div class="service-details">
+        <aside>
+            Friday: Erev Shabbat<br>
+            7:00pm to 8:30pm<br>
+            Shabbat Candle Lighting<br>
+            Prayer Service<br>
+            Kiddush Shabbat Study
+        </aside>
+        <aside>
+            Saturday: Shabbat<br>
+            10:30am to 2:00pm<br>
+            Prayer Service<br>
+            Torah (God’s Law) Reading<br>
+            Songs of Praise<br>
+            D’rashah (Sermon)<br>
+            Oneg (Refreshments)
+        </aside>
+        <aside>
+            Saturday Evening<br>
+            4:00pm to 6:00pm<br>
+            Prayer and Pledge<br>
+            Prophetic, current and historical events from a spiritual point of view.
+        </aside>
+    </div>
+
+</section>
+
+<section class="section events-section">
+    <div class="title">UPCOMING EVENTS</div>
+    <div class="events-details">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/rect.svg" alt="placeholder" />
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/rect.svg" alt="placeholder" />
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/rect.svg" alt="placeholder" />
+    </div>
+    <a href="#">View All In Calendar</a>
+</section>
+
+<section class="section donate-section">
+    <div class="title">DONATE TO OUR CAUSE</div>
+    <div class="donate-details">
+        <button class="donate-button">DONATE TODAY</button>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</p>
+    </div>
+</section>
