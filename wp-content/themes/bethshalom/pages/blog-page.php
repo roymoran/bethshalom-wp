5e46b5008c2f6f41f0a6686b3d61b4248cf874da
Edgar <mayedgar22@gmail.com>
2019-08-19 18:59:51 -0500

    code for blog layout

---
+++ wp-content/themes/bethshalom/pages/blog-page.php
@@ -0,0 +1,45 @@
+<section class="page-header">
+    <div>OUR BLOG</div>
+    <hr>
+</section>
+
+<section class="info-section">
+    <p class="info-section-content">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis, risus nulla dictumst aliquam justo interdum dapibus dignissim varius, non massa sapien dictum metus ac scelerisque. Curae ornare curabitur lacinia gravida metus primis pharetra inceptos rhoncus fringilla ligula, senectus maecenas et hendrerit venenatis lacus porttitor etiam tempor nullam, integer ut dignissim neque aptent magna sed risus platea nascetur.</p>
+</section>
+
+
+<div class="blog">
+  <div class="placeimg"></div>
+  <h1 class ="blog-title">Blog Title</h1>
+  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+  </div>
+    
+<div class="blog">
+  <div class="placeimg"></div>
+  <h1 class ="blog-title">Blog Title</h1>
+  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+  </div>
+      
+<div class="blog">
+  <div class="placeimg"></div>
+  <h1 class ="blog-title">Blog Title</h1>
+  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+  </div>
+
+<div class="blog">
+  <div class="placeimg"></div>
+  <h1 class ="blog-title">Blog Title</h1>
+  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+  </div>
+      
+<div class="blog">
+  <div class="placeimg"></div>
+  <h1 class ="blog-title">Blog Title</h1>
+  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+  </div>
+      
