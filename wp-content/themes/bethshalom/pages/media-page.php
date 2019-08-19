3c15a60a4bfa3d4abf647723deb9af80b2ea6d82
Roy Moran <roy_moran1993@yahoo.com>
2019-08-19 21:33:05 +0200

    add media page

---
+++ wp-content/themes/bethshalom/pages/media-page.php
@@ -0,0 +1,40 @@
+<section class="page-header">
+    <div>MEDIA</div>
+    <hr>
+</section>
+
+<section class="section media-section">
+    <div class="title">HINT MAGAZINE</div>
+    <div class="magazine-section">
+        <a href="#">View the latest issue by clicking here</a>
+    </div>
+    <div class="title">RADIO</div>
+    <div class="radio-section">
+        <a href="#" target="_blank">LINK 1</a>
+        <a href="#" target="_blank">LINK 2</a>
+        <a href="#" target="_blank">LINK 3</a>
+    </div>
+    <div class="title">VIDEO</div>
+    <div class="video-section">
+        <a href="#" target="_blank">LINK 1</a>
+        <a href="#" target="_blank">LINK 2</a>
+        <a href="#" target="_blank">LINK 3</a>
+    </div>
+    <div class="title">GALLERY</div>
+    <div class="gallery-section">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/left-arrow.svg" alt="placeholder" />
+        <div class="gallery">
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+            <img class="gallery-img" src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+        </div>
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/right-arrow.svg" alt="placeholder" />
+    </div>
+</section>
