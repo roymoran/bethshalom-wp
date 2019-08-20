1d7c293fe954a6548d7c850d8d5941280dbe9264
Edgar <mayedgar22@gmail.com>
2019-08-19 19:17:51 -0500

    layout testing 2

--- wp-content/themes/bethshalom/pages/blog-page.php
+++ wp-content/themes/bethshalom/pages/blog-page.php
@@ -1,10 +1,10 @@
-<section class="page-header">
+<section class="blog-header">
     <div>OUR BLOG</div>
     <hr>
 </section>
 
-<section class="info-section">
-    <p class="info-section-content">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis, risus nulla dictumst aliquam justo interdum dapibus dignissim varius, non massa sapien dictum metus ac scelerisque. Curae ornare curabitur lacinia gravida metus primis pharetra inceptos rhoncus fringilla ligula, senectus maecenas et hendrerit venenatis lacus porttitor etiam tempor nullam, integer ut dignissim neque aptent magna sed risus platea nascetur.</p>
+<section class="blog-info-section">
+    <p class="blog-info-section-content">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis, risus nulla dictumst aliquam justo interdum dapibus dignissim varius, non massa sapien dictum metus ac scelerisque. Curae ornare curabitur lacinia gravida metus primis pharetra inceptos rhoncus fringilla ligula, senectus maecenas et hendrerit venenatis lacus porttitor etiam tempor nullam, integer ut dignissim neque aptent magna sed risus platea nascetur.</p>
 </section>
 
 
