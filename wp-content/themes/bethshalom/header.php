890b13d5d06760948294ccfcd895c10b1b620036
Roy Moran <roy_moran1993@yahoo.com>
2019-08-21 13:57:09 -0500

    header changes (#32)

--- wp-content/themes/bethshalom/header.php
+++ wp-content/themes/bethshalom/header.php
@@ -9,7 +9,7 @@
 <body <?php body_class();?>>
 <div class="header">
   <img src="http://bethshalombz.org/blog2/wp-content/uploads/2010/02/brotherhood_logo1.jpg" alt="Beth Shalom Logo" align="left">
-  <h1>Beth Shalom B'Nai Zaken</h1>
+  <h1>Beth Shalom B'Nai Zaken<br>EHC</h1>
 
 </div>
 <!-- Navigation Bar --> 
