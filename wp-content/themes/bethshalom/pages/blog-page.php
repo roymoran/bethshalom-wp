ea791ede3290c246e689f2529e76466073d4fc4a
Roy Moran <roy_moran1993@yahoo.com>
2019-08-21 16:06:39 -0500

    changes to blog page

--- wp-content/themes/bethshalom/pages/blog-page.php
+++ wp-content/themes/bethshalom/pages/blog-page.php
@@ -1,45 +1,35 @@
-<section class="blog-header">
-    <div>OUR BLOG</div>
-    <hr>
-</section>
-
-<section class="blog-info-section">
-    <p class="blog-info-section-content">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis, risus nulla dictumst aliquam justo interdum dapibus dignissim varius, non massa sapien dictum metus ac scelerisque. Curae ornare curabitur lacinia gravida metus primis pharetra inceptos rhoncus fringilla ligula, senectus maecenas et hendrerit venenatis lacus porttitor etiam tempor nullam, integer ut dignissim neque aptent magna sed risus platea nascetur.</p>
+<section class="page-header">
+  <div>BLOG</div>
+  <hr>
 </section>
 
 
-<div class="blog">
-  <div class="placeimg"></div>
-  <h1 class ="blog-title">Blog Title</h1>
-  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
-  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
-  </div>
-    
-<div class="blog">
-  <div class="placeimg"></div>
-  <h1 class ="blog-title">Blog Title</h1>
-  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
-  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
-  </div>
-      
-<div class="blog">
-  <div class="placeimg"></div>
-  <h1 class ="blog-title">Blog Title</h1>
-  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
-  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+<section class="section blog-section">
+  <p class="blog-section-header">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis, risus nulla dictumst aliquam justo interdum dapibus dignissim varius, non massa sapien dictum metus ac scelerisque. Curae ornare curabitur lacinia gravida metus primis pharetra inceptos rhoncus fringilla ligula, senectus maecenas et hendrerit venenatis lacus porttitor etiam tempor nullam, integer ut dignissim neque aptent magna sed risus platea nascetur.</p>
+  <div class="blog-post">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+    <div class="blog-post-details">
+    <div class="blog-title">Blog Title</div>
+      <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+      <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+    </div>
   </div>
 
-<div class="blog">
-  <div class="placeimg"></div>
-  <h1 class ="blog-title">Blog Title</h1>
-  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
-  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+  <div class="blog-post">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+    <div class="blog-post-details">
+    <div class="blog-title">Blog Title</div>
+      <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+      <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+    </div>
   </div>
-      
-<div class="blog">
-  <div class="placeimg"></div>
-  <h1 class ="blog-title">Blog Title</h1>
-  <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
-  <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+
+  <div class="blog-post">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery-square-placeholder.svg" alt="placeholder" />
+    <div class="blog-post-details">
+      <div class="blog-title">Blog Title</div>
+      <p class="blog-sample">Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur mauris platea praesent, proin nec sem gravida fusce eleifend morbi duis class felis ornare, ligula massa dis suspendisse litora venenatis cum lobortis at ultricies. Mollis vitae nisi torquent primis purus cras fermentum mattis...</p>
+      <p class="blog-link"><a href="http://bethshalombz.org">Read More...</a></p>
+    </div>
   </div>
-      
+</section>
