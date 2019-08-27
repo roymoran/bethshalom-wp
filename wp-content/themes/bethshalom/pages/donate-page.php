705c4d444dc2e53b49cb0acbf3ec1de3d5e9d2e5
hfernandez <hfernandez09829@gmail.comm>
2019-08-27 17:21:29 -0500

    add donate and contact pages

---
+++ wp-content/themes/bethshalom/pages/donate-page.php
@@ -0,0 +1,60 @@
+<div class="a">
+<h1 style="clear:both"><u>DONATE TO OUR CAUSE</u></h1>
+<br>
+<p>Beth Sahlom B’Nai Zaken EHC’s operation and outreach activities are wholly supported by giving members and friends. Contributions can be made from this page via Paypal to one of the following funds.</p>
+
+
+<div class="b">
+<h2>Tithe</h2>
+</div>
+
+<p>An easy way for members to make sure their tithes are current.</p>
+<br>
+
+<a href="https://www.paypal.com/donate/?token=g5Ykm6IDw6a_YdA6gpLl2uDvBExxPv6-ItEBuw04foKrNV5yL5yjvRm1mDbH8SsZpTwaZW&country.x=US&locale.x=US&Z3JncnB0=
+" class="button">Donate Today</a>
+
+
+<div class="b">
+<h2>General Fund</h2>
+</div>
+
+<p> For the daily operation of the synagogue. </p>
+<br>
+<a href="https://www.paypal.com/donate/?token=VxY4zarnQEjVv9VW5g9HrzrVwCLSII4uiTMoPkfgcqgiW9AkiJIS282PPWDnY2O5sE3VlW&country.x=US&locale.x=US
+" class="button">Donate Today</a>
+
+<div class="b">
+<h2>Rabbi Hailu Paris Scholarship Fund</h2>
+</div>
+
+<p> Please help us continue Rabbi Paris’ legacy of educating others. </p>
+<br>
+<a href="" class="button">Donate Today</a>
+
+<div class="b">
+<h2>New Sefer Torah Fund</h2>
+</div>
+  
+<p> For the purchase of our own Sefer Torah which is currently being written. </p>
+<br>
+<a href="https://www.paypal.com/donate/?token=0wM43LGF3xzRMoMLit4NUX0vYSp1WVoxhftHzUdCgidPx3_gwzEeYGoyfmFrZK_Ltd9RdW&country.x=US&locale.x=US
+" class="button">Donate Today</a>
+
+<div class="b">
+<h2>Sacred Aging Fund</h2>
+</div>
+    
+<p>Senior Program operated for the community at-large with emphasis placed on the socializing benefit of interpersonal communication with group activities which encourage the seniors to socialize and interact, and discourage withdrawal and isolation. </p>
+<br>
+<a href="https://www.paypal.com/donate/?token=dV6i1KzLHpJ2sAozgIB6DSkKLgKe8FneEdDUIj4HmL0QlfO8h6oTSDNrVhLWiDXFkjKNlW&country.x=US&locale.x=US&Z3JncnB0=
+" class="button">Donate Today</a>
+
+<div class="b">
+<h2>Israelite Academy Tuition</h2>
+</div>
+<br>
+<a href="https://www.paypal.com/webapps/shoppingcart?flowlogging_id=79db39d048a49&mfid=1566344015610_79db39d048a49#/checkout/openButton
+" class="button">Buy Now</a>
+
+</div>
