888fc13669051a79fb88fa0a157873dce053ff74
Roy Moran <roy_moran1993@yahoo.com>
2019-09-02 00:42:07 -0500

    replace placeholder content

--- wp-content/themes/bethshalom/pages/about-page.php
+++ wp-content/themes/bethshalom/pages/about-page.php
@@ -7,15 +7,62 @@
     <div class="title">LEADERSHIP AT BETH SHALOM</div>
     <div class="leader">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/circle-placeholder.svg" alt="placeholder" />
-        <div class="leader-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        <div class="leader-bio">
+            <span>RABBI AVRAHAM</span>
+            <br>
+            <br>
+            I am a graduate of the Israelite Rabbinical Academy which is based in New York City. I graduated and was ordained June 25, 2005 at Beth Shalom B’nai Zaken Ethiopian Hebrew Congregation. It was a momentous occasion because there were no black Rabbis ordained in Chicago since 1992. It is truly an honor to carry on the legacy of our Great Rabbis who preceded me. I am currently a member here where I serve as Gabbai, Baal Koreh and lead prayer services on various Sabbaths. I bring the message every fourth Sabbath of the month. Additional dutiesincludes captain of Shomreem (security) and drummer for our band. I am available for lectures and consultation and can be reached via e-mail. Avrahamb12@sbcglobal.net
+            <br>
+            <br>
+            I attended DeVry Institute of Technology where I received an Associates in Applied Electronics Degree. I graduated from DeVry in 1989 and became employed with Motorola Corporation until current. I became very knowledgeable in the area of real estate when I became a realtor. I was licensed to sell life insurance but I did not pursue it. The wealth of financial information I attained while doing the business was great.
+            <br>
+            <br>
+            Personal comments
+            <br>
+            I am delighted to be doing the service of the Most High. I get great pleasure out of influencing people’s lives in a positive way and sharing my knowledge of Torah to the community. I am very anxious to get deeper involved in the growth of our community. This is a life long study for me but I know that as long as we have Great men around such as our Chief Rabbi Cappers Funnye and the Elders of our community, I am sure I will continue to improve my knowledge of Torah and become very effective in the Israelite community.
+            <br>
+            <br>
+            I am married to a very beautiful woman Leah Baht Ysrael (Louveria Burks) who has been very instrumental in my life. I have two wonderful daughters Monique and Dominique whom I love very much. And last but certainly not least a beautiful Grandbaby Kamari who is so adorable. I enjoy reading, basketball and playing the drums and I look forward to traveling the world to continue the work of the Most High.
+        </div>
     </div>
     <div class="leader">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/circle-placeholder.svg" alt="placeholder" />
-        <div class="leader-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        <div class="leader-bio">
+            <span>RABBI FUNNYE</span>
+            <br>
+            <br>
+            Capers C. Funnye, Jr. is rabbi and spiritual leader of Beth Shalom B’nai Zaken Ethiopian Hebrew Congregation, located in Chicago, IL at 6601 S. Kedzie Avenue. Rabbi Funnye also serves as a Senior Research Associate for the Institute of Jewish and Community Research, located in San Francisco, CA.
+            <br>
+            <br>
+            Rabbi Funnye earned a Bachelor of Arts in Hebrew Literature and rabbinic ordination from the Israelite Board of Rabbis, Inc., Queens, NY. Rabbi Funnye also earned a Bachelor of Arts degree in Jewish Studies and Master of Science in Human Service Administration from Spertus Institute of Jewish Studies, Chicago, IL.
+            <br>
+            <br>
+            Rabbi Funnye has served as a consultant to several institutions, including The Du Sable Museum of African American History, The Chicago Historical Society, The Spertus Museum of Judaica, all located in Chicago, IL; The Black Holocaust Museum, located in Milwaukee, WI; Institute for Jewish and Community Research, San Francisco, CA and the Afro-American Museum, located in Los Angeles, CA.
+            <br>
+            <br>
+            Rabbi Funnye has lectured at numerous universities, synagogues, churches and various community organizations throughout the United States. He has appeared on several national and local television programs, and spoken on numerous radio programs both national and local.
+            <br>
+            <br>
+            Rabbi Funnye is involved in a number of boards in the Jewish community; The Chicago Board of Rabbis, Jewish Council on Urban Affairs, Akiba Schechter Jewish Day School and Vice President of the Israelite Board of Rabbis. Rabbi Funnye is married and he and his wife Mary have four children and are the proud grandparents of thirteen.
+            <br>
+            <br>
+            Rabbi Funnye is available for lectures and discussion groups
+        </div>
     </div>
     <div class="leader">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/circle-placeholder.svg" alt="placeholder" />
-        <div class="leader-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        <div class="leader-bio">
+            <span>RABBI YAHATH</span>
+            <br>
+            <br>
+            Rabbi Yahath was born June 20, 1949 in Gary, Indiana. He attended Roosevelt High School. Upon completion of high school, Yahath enlisted in the United States Marine Corps. During his enlistment he served in Viet Nam from 1968-1969. Before being honorably discharged, received the Purple Heart Medal. Yahath recently retired after 25 years of employment the Veteran’s Administration.
+            <br>
+            <br>
+            Yahath returned to the Hebrew way of life in the spring of 1972. He was a part of various Israelite communities extending from Chicago to St. Croix, Virgin Islands, before deciding to settle at Beth Shalom B’Nai Zaken E.H.C in 1990. Since becoming a member of Beth Shalom, Yahath has served as the Anshe Chayil President, Shomreim and a percussionist with the band.
+            <br>
+            <br>
+            Rabbi Yahath was ordained on June 23, 2012. He is married to Beth Brazelton. <br>
+        </div>
     </div>
 </section>
 
@@ -23,18 +70,46 @@
     <div class="title">OUR NOTABLE EVENTS</div>
     <div class="event">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/square-placeholder.svg" alt="placeholder" />
-        <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        <div class="description">
+            <a href="#" class="title">
+                Featured in the New York Times. Click here to read.
+            </a>
+            <div class="details">
+                “Rabbi Capers Funnye celebrated Martin Luther King Day this year in New York City at the Stephen Wise Free Synagogue, a mainstream Reform congregation, in the company of about 700 fellow Jews — many of them black. The organizers of the event had reached out to four of New York’s Black Jewish synagogues in the hope of promoting Jewish diversity, and they weren’t disappointed...”
+            </div>
+        </div>
     </div>
     <div class="event-reverse">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/square-placeholder.svg" alt="placeholder" />
-        <div class="description-reverse">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        <div class="description-reverse">
+            <a href="#" class="title">
+                Featured in the New York Times. Click here to read.
+            </a>
+            <div class="details">
+                “CHICAGO — Having grown up in the African Methodist Episcopal Church, Capers C. Funnye Jr. was encouraged by his pastor to follow in his footsteps. Instead, he became a rabbi...”
+            </div>
+        </div>
     </div>
     <div class="event">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/square-placeholder.svg" alt="placeholder" />
-        <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        <div class="description">
+            <a href="#" class="title">
+                Click here to read full blog post
+            </a>
+            <div class="details">
+                Monday December 10, 2007 was a water-shed day for the Israelite community in the Unites States. My wife Rabbinit Miriam and I attended a reception at the White House in honor of Chanukah. I was very surprised when I received a call from Jeremy Katz, the White House Deputy-Chief-of-Staff for Jewish affairs. We talked about my attending a private meeting with President Bush. In light of the fact that December was Human Rights Month, President Bush wanted to conduct a panel discussion on religious oppression.
+            </div>
+        </div>
     </div>
     <div class="event-reverse">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/square-placeholder.svg" alt="placeholder" />
-        <div class="description-reverse">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et odio at leo pulvinar finibus. Cras eu iaculis est. Sed in dapibus sem, vel aliquet neque. Sed sed mauris porta, tempor ex non, tempor purus. Pellentesque commodo facilisis tempus. Curabitur placerat elementum porta. Aenean dictum dui ex, vitae mattis ligula feugiat a.</div>
+        <div class="description-reverse">
+            <a href="#" class="title">
+                Featured in Forward. Click here to read.
+            </a>
+            <div class="details">
+                Voices in Praise: Congregants gathered at Bnai Adath Kol Beth Yisrael, an Israelite synagouge in Brooklyn, sing during the inauguration ceremony of Rabbi Funnye.
+            </div>
+        </div>
     </div>
 </section>
--- wp-content/themes/bethshalom/pages/media-page.php
+++ wp-content/themes/bethshalom/pages/media-page.php
@@ -10,15 +10,48 @@
     </div>
     <div class="title">RADIO</div>
     <div class="radio-section">
-        <a href="#" target="_blank">LINK 1</a>
-        <a href="#" target="_blank">LINK 2</a>
-        <a href="#" target="_blank">LINK 3</a>
+        <div class="details">Black Rabbi Shares Story Of Conversion, Unity</div>
+        <div class="details">April 10, 2009</div>
+        <div class="details">Rabbi Capers C. Funnye grew up Methodist, but he converted to Judaism and is now a Rabbi. Funnye now leads an Ethiopian Hebrew Congregation in Chicago, one of the largest Black synagogues in America (and he’s also First Lady Michelle Obama’s cousin).</div>
+        <br>
+        <div class="details">
+            Rabbi Funnye shares his faith journey and his work to unite black and white Jewish communities. Listen Here
+        </div>
+        <br>
+        <a href="#" target="_blank">Listen Here</a>
     </div>
     <div class="title">VIDEO</div>
     <div class="video-section">
-        <a href="#" target="_blank">LINK 1</a>
-        <a href="#" target="_blank">LINK 2</a>
-        <a href="#" target="_blank">LINK 3</a>
+        <div class="details"><span>Watch us featured on WTTW Chicago Tonight.</span> We are also featured on the following segments, seperated into different parts due to program length:</div>
+        <div class="show-list">
+            <div class="show-details">
+                <div class="show-title">Aaron Freeman Show:</div>
+                <div><a href="#">- Part 1</a></div>
+                <div><a href="#">- Part 2</a></div>
+                <div><a href="#">- Part 3</a></div>
+            </div>
+
+            <div class="show-details">
+                <div class="show-title">Aaron Freeman Show Again:</div>
+                <div><a href="#">- Part 1</a></div>
+                <div><a href="#">- Part 2</a></div>
+                <div><a href="#">- Part 3</a></div>
+            </div>
+
+            <div class="show-details">
+                <div class="show-title">Common Ground:</div>
+                <div><a href="#">- Part 1</a></div>
+                <div><a href="#">- Part 2</a></div>
+                <div><a href="#">- Part 3</a></div>
+            </div>
+
+            <div class="show-details">
+                <div class="show-title">Our Voices:</div>
+                <div><a href="#">- Part 1</a></div>
+                <div><a href="#">- Part 2</a></div>
+                <div><a href="#">- Part 3</a></div>
+            </div>
+        </div>
     </div>
     <div class="title">GALLERY</div>
     <div class="gallery-section">
