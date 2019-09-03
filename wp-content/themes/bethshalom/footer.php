b5b76cf7ca24b9e045631e3830baef95fa830dc3
Roy Moran <roy_moran1993@yahoo.com>
2019-09-02 23:51:45 -0500

    add wp footer links (#40)

--- wp-content/themes/bethshalom/footer.php
+++ wp-content/themes/bethshalom/footer.php
@@ -1,4 +1,4 @@
-<?php wp_footer();?>
+<?php wp_footer(); ?>
 
 <footer>
 
@@ -7,41 +7,22 @@
         <a href="https://www.youtube.com/user/BethShalomBZ" class="social-icon">
             <img src="https://seeklogo.net/wp-content/uploads/2016/06/YouTube-icon-400x400.png" width="52" height="56" title="YouTube" alt="YouTube"></a>
         <a href="https://www.facebook.com/bethshalombz.org/" class="social-icon">
-            <img src="https://seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview-1-400x400.png" width="52" height="56" title="Facebook" alt="Facebook">
+            <img src="https://ie.reachout.com/wp-content/uploads/2015/02/Facebook.png" width="52" height="56" title="Facebook" alt="Facebook">
         </a>
     </div>
-    <div>
-        <h5 class="links-title"> Useful Links </h5>
-        <h5 align="center"><a href="#" > Nav Item 1 </h5></a>
-        <h5 align="center"><a href="#" > Nav Item 2 </h5></a>
-        <h5 align="center"><a href="#" > Nav Item 3 </h5></a>
-        <h5 align="center"><a href="#" > Nav Item 4 </h5></a>
-        <h5 align="center"><a href="#" > Nav Item 5 </h5></a>
-        <h5 align="center"><a href="#" > Nav Item 6 </h5></a>
+    <div class="footer-nav-links-container">
+        <div class="footer-links-title">USEFUL LINKS</div>
+        <?php wp_nav_menu(array('theme_location' => 'footer-menu', 'container' => 'div', 'container_class' => '', 'menu_class' => 'footer-nav-links')); ?>
     </div>
 
     <div class="map-container">
         <h5 class="address"> 6601 South Kedzie Avenue <br> Chicago, Il 60629-3432 <br>(773)476-2924<br> Email: <br> bshalombz@sbcglobal.net<br> Rabbi:<br> ravfunnt@sbcglobal.net</h5>
-        <div id="wrapper-9cd199b9cc5410cd3b1ad21cab2e54d3">
-		    <div id="map-9cd199b9cc5410cd3b1ad21cab2e54d3">        
-            <script>
-                (function () {
-                var setting = {"height":260,"width":305,"zoom":8,"queryString":"6601 South Kedzie Avenue, Chicago, IL, USA","place_id":"ChIJvyHXBywwDogRSbtKl3RRs4U","satellite":false,"centerCoord":[41.773272164185336,-87.70277924999999],"cid":"0x85b35174974abb49","cityUrl":"/us/il/chicago","id":"map-9cd199b9cc5410cd3b1ad21cab2e54d3","embed_id":"62446"};
-                var d = document;
-                var s = d.createElement('script');
-                s.src = 'https://1map.com/js/script-for-user.js?embed_id=62446';
-                s.async = true;
-                s.onload = function (e) {
-                  window.OneMap.initMap(setting)
-                };
-                var to = d.getElementsByTagName('script')[0];
-                to.parentNode.insertBefore(s, to);
-                })();
-            </script>
-            <a href="https://1map.com/map-embed?embed_id=62446">1map.com</a>
-            </div>
+        <div>
+            <iframe width="320" height="250" frameborder="0" src="https://www.bing.com/maps/embed?h=250&w=320&cp=41.77313978979112~-87.70263&lvl=16&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
+            </iframe>
         </div>
     </div>
 </footer>
 </body>
+
 </html>
