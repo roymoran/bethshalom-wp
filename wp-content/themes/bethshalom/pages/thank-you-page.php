badb7f89e43323e76f01bd8be25c738dd511b166
Roy Moran <roy_moran1993@yahoo.com>
2019-09-28 03:13:03 -0500

    add thank you page (#72)

---
+++ wp-content/themes/bethshalom/pages/thank-you-page.php
@@ -0,0 +1,37 @@
+<section class="page-header" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/committees70.jpg');">
+  <div>THANK YOU</div>
+  <hr>
+</section>
+
+
+<section class="section">
+  <div class="thank-you-message">A special “thank you” to the following for their efforts on the redesign of our website:</div>
+</section>
+<section class="section thank-you-lists">
+  <div class="thank-you-list">
+    <ul>
+      <li>The Hecktman Family</li>
+      <li>Nuevo Foundation</li>
+      <li>Microsoft</li>
+    </ul>
+  </div>
+  <div class="thank-you-list">
+
+    <ul>
+      <li>Chicago Public Schools</li>
+      <li>DePaul University</li>
+      <li>Mind + Hand</li>
+    </ul>
+  </div>
+</section>
+
+<section class="section logos-section">
+  <img src="<?php echo get_template_directory_uri() ?>/assets/images/mh-logo.png" alt="Mind and Hand" width="283" />
+  <img src="<?php echo get_template_directory_uri() ?>/assets/images/ms-logo.png" alt="Microsoft" width="283" />
+  <img src="<?php echo get_template_directory_uri() ?>/assets/images/nuevofoundation-logo.png" alt="Nuevo Foundation" width="283" />
+</section>
+
+<section class="section logos-section">
+  <img src="<?php echo get_template_directory_uri() ?>/assets/images/dpu-logo.png" alt="DePaul University" width="283" />
+  <img src="<?php echo get_template_directory_uri() ?>/assets/images/cps-logo.png" alt="Microsoft" width="283" />
+</section>
