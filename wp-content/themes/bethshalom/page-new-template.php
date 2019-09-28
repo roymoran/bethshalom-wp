70c508966099472d282d795723a4671ab2a9b1cf
Roy Moran <roy_moran1993@yahoo.com>
2019-09-27 23:27:05 -0500

    add new page template

---
+++ wp-content/themes/bethshalom/page-new-template.php
@@ -0,0 +1,29 @@
+<?php /* Template Name: NewPageTemplate */ ?>
+<?php get_header(); ?>
+
+<?php
+if (have_posts()) {
+
+    // Load posts loop.
+    while (have_posts()) {
+        the_post();
+        ?>
+        <section class="page-header" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/commitees70.jpg);">
+            <div class="new-page-template-title"> <?php the_title() ?> </div>
+            <hr>
+        </section>
+        <section class="section committee-section">
+            <?php
+                if (get_the_post_thumbnail_url()) {
+                    echo "<img src='<?php get_the_post_thumbnail_url() ?>' alt='page-image' width='300px;' height='300px;' />";
+                }
+            ?>
+            <div class="committee-info"><?php the_content() ?></div>
+        </section>
+<?php
+
+    }
+} else { }
+
+?>
+<?php get_footer(); ?>
